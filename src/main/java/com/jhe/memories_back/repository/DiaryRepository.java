46d86707460a7c0503686fb048c1848fb5454ed1
jhe2426 <jhe2426@naver.com>
2025-09-08 19:40:59 +0900

    fix: Repository 어노테이션 추가

--- src/main/java/com/jhe/memories_back/repository/DiaryRepository.java
+++ src/main/java/com/jhe/memories_back/repository/DiaryRepository.java
@@ -1,9 +1,11 @@
 package com.jhe.memories_back.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.jhe.memories_back.common.entity.DiaryEntity;
 
+@Repository
 public interface DiaryRepository extends JpaRepository<DiaryEntity, Integer> {
     
 }
