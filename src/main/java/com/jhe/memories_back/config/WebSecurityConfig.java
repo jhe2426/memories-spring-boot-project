bd8073d32a786a0ac3014afb22f9f25723f8f46c
jhe2426 <jhe2426@naver.com>
2025-09-10 20:27:14 +0900

    feat: WebSecurityConfig 구현 (기본 시큐리티 설정 추가)

---
+++ src/main/java/com/jhe/memories_back/config/WebSecurityConfig.java
@@ -0,0 +1,65 @@
+package com.jhe.memories_back.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import com.jhe.memories_back.filter.JwtAuthenticationFilter;
+
+import lombok.RequiredArgsConstructor;
+
+// description: Bearer 인증 방식을 사용하기 위해 Basic 인증 미사용 //
+// description: REST API 서버로 Session 유지하지 않음 //
+// description: CORS 정책은 모든 출처 및 리소스에 대해서 허용 //
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class WebSecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    
+    // function: Web Security 설정 메서드 //
+    @Bean
+    protected SecurityFilterChain configure(HttpSecurity security) throws Exception {
+
+        security
+            // description: Basic 인증 미사용 지정 //
+            .httpBasic(HttpBasicConfigurer::disable)
+            // description: Session 유지하지 않음 지정 //
+            .sessionManagement(management -> management
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            )
+            // description: csrf 취약점 대비 미사용 지정 //
+            .csrf(CsrfConfigurer::disable)
+            // description: CORS 정책 설정 //
+            .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+            // description: JwtAuthentication Filter 등록 //
+            .addFilterBefore( jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return security.build();
+    }
+
+    // function: CORS 정책 설정 객체를 반환하는 메서드 //
+    @Bean
+    protected CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
+        configuration.addAllowedOrigin("*");
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration( "/**", configuration);
+
+        return source;
+    }
+
+}
