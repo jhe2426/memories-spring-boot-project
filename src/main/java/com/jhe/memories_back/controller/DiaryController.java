f093e3a4bd2699983db61f5569f574359810888f
jhe2426 <jhe2426@naver.com>
2025-09-15 21:35:48 +0900

    feat(diary): 사용자 일기 삭제 API 구현

--- src/main/java/com/jhe/memories_back/controller/DiaryController.java
+++ src/main/java/com/jhe/memories_back/controller/DiaryController.java
@@ -2,13 +2,16 @@ package com.jhe.memories_back.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.jhe.memories_back.common.dto.request.diary.PatchDiaryRequestDto;
 import com.jhe.memories_back.common.dto.request.diary.PostDiaryRquestDto;
 import com.jhe.memories_back.common.dto.response.ResponseDto;
 import com.jhe.memories_back.common.dto.response.diary.GetDiaryResponseDto;
@@ -42,11 +45,30 @@ public class DiaryController {
         return response;
     }
 
-    @GetMapping("{diaryNumber}")
+    @GetMapping("/{diaryNumber}")
     public ResponseEntity<? super GetDiaryResponseDto> getDiary(
         @PathVariable("diaryNumber") Integer diaryNumber
     ) {
         ResponseEntity<? super GetDiaryResponseDto> response = diaryService.getDiary(diaryNumber);
         return response;
+    }
+
+    @PatchMapping("/{diaryNumber}")
+    public ResponseEntity<ResponseDto> patchDiary(
+        @RequestBody @Valid PatchDiaryRequestDto requestBody,
+        @PathVariable("diaryNumber") Integer diaryNumber,
+        @AuthenticationPrincipal String userId
+    ) {
+        ResponseEntity<ResponseDto> response = diaryService.patchDiary(requestBody, diaryNumber, userId);
+        return response;
+    }
+
+    @DeleteMapping("/{diaryNumber}")
+    public ResponseEntity<ResponseDto> deleteDiary(
+        @PathVariable("diaryNumber") Integer diaryNumber,
+        @AuthenticationPrincipal String userId
+    ) {
+        ResponseEntity<ResponseDto> resposne = diaryService.deleteDiary(diaryNumber, userId);
+        return resposne;
     }
 }
