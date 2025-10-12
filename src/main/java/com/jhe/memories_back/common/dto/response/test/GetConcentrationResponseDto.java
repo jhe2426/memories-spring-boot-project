4b9e3fa75bbf922562308a89b91f86f0fed1038d
jhe2426 <jhe2426@naver.com>
2025-10-12 09:09:57 +0900

    feat(test): 집중력 기록 목록 조회 API 구현

---
+++ src/main/java/com/jhe/memories_back/common/dto/response/test/GetConcentrationResponseDto.java
@@ -0,0 +1,27 @@
+package com.jhe.memories_back.common.dto.response.test;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import com.jhe.memories_back.common.dto.response.ResponseDto;
+import com.jhe.memories_back.common.entity.ConcentrationTestEntity;
+import com.jhe.memories_back.common.vo.ConcentrationTestVO;
+
+import lombok.Getter;
+
+@Getter
+public class GetConcentrationResponseDto extends ResponseDto {
+    
+    private List<ConcentrationTestVO> concentrationTests;
+
+    private GetConcentrationResponseDto(List<ConcentrationTestEntity> concentrationTestEntities) {
+        this.concentrationTests = ConcentrationTestVO.getList(concentrationTestEntities);
+    }
+
+    public static ResponseEntity<GetConcentrationResponseDto> success(List<ConcentrationTestEntity> concentrationTestEntities) {
+        GetConcentrationResponseDto body = new GetConcentrationResponseDto(concentrationTestEntities);
+        return ResponseEntity.status(HttpStatus.OK).body(body);
+    }
+}
--- src/main/java/com/jhe/memories_back/repository/ConcentrationTestRepository.java
+++ src/main/java/com/jhe/memories_back/repository/ConcentrationTestRepository.java
@@ -1,5 +1,7 @@
 package com.jhe.memories_back.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,4 +13,5 @@ public interface ConcentrationTestRepository extends JpaRepository<Concentration
     
     Integer countByUserId(String userId);
     ConcentrationTestEntity findByUserIdAndSequence(String userId, Integer sequence);
+    List<ConcentrationTestEntity> findByUserIdOrderBySequenceDesc(String userId);
 }
