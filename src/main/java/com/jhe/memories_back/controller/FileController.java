195f2ef793af966e366406f1ec251bd587b3cefb
jhe2426 <jhe2426@naver.com>
2025-10-21 14:52:27 +0900

    feat(file): 파일 업로드 API 구현

---
+++ src/main/java/com/jhe/memories_back/controller/FileController.java
@@ -0,0 +1,28 @@
+package com.jhe.memories_back.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.jhe.memories_back.service.FileService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/file")
+@RequiredArgsConstructor
+public class FileController {
+
+    private final FileService fileService;
+ 
+    @PostMapping("/upload")
+    public String upload(
+        @RequestParam("file") MultipartFile file
+    ) {
+        String url = fileService.upload(file);
+        return url;
+    }
+
+}
---
+++ src/main/java/com/jhe/memories_back/service/FileService.java
@@ -0,0 +1,9 @@
+package com.jhe.memories_back.service;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public interface FileService {
+    
+    String upload(MultipartFile file);
+
+}
