c2cb57c85538612a8e7ca73729ee2847e363f658
jhe2426 <jhe2426@naver.com>
2025-10-22 21:18:20 +0900

    feat(file): 파일 가져오는 API 구현

--- src/main/java/com/jhe/memories_back/controller/FileController.java
+++ src/main/java/com/jhe/memories_back/controller/FileController.java
@@ -1,5 +1,9 @@
 package com.jhe.memories_back.controller;
 
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -25,4 +29,11 @@ public class FileController {
         return url;
     }
 
+    @GetMapping(value="/{fileName}", produces={MediaType.IMAGE_PNG_VALUE, MediaType.IMAGE_JPEG_VALUE})
+    public Resource getImageFile(
+        @PathVariable("fileName") String fileName
+    ) {
+        Resource resource = fileService.getImageFile(fileName);
+        return resource;
+    }
 }
--- src/main/java/com/jhe/memories_back/service/FileService.java
+++ src/main/java/com/jhe/memories_back/service/FileService.java
@@ -1,9 +1,11 @@
 package com.jhe.memories_back.service;
 
+import org.springframework.core.io.Resource;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface FileService {
     
     String upload(MultipartFile file);
+    Resource getImageFile(String fileName);
 
 }
